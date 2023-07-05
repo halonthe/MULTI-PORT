55d9f3d54e000611a49546755a604443273793b3
halonthe <80936114+halonthe@users.noreply.github.com>
2023-07-05 20:46:03 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -259,6 +259,8 @@ echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - SSH Websocket           : 80 [ON]" | tee -a log-install.txt
 echo "   - SSH SSL Websocket       : 443" | tee -a log-install.txt
+echo "   - L2TP                    : 1701" | tee -a log-install.txt
+echo "   - PPTP                    : 1732" | tee -a log-install.txt
 echo "   - Stunnel4                : 447, 777" | tee -a log-install.txt
 echo "   - Dropbear                : 109, 143" | tee -a log-install.txt
 echo "   - Badvpn                  : 7100-7900" | tee -a log-install.txt
